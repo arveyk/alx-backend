d10b932391daec16921978404fe6728ce3d769cb
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-28 18:09:46 +0300

    Get method implements the counting, credit to Mokhtar Ramadan

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
 """FIFOCache
 """
-from collections import Counter
 BaseCaching = __import__('0-basic_cache').BaseCaching
-
-
-def CallCount(func):
-    """Counter function
-    Args:
-        args:
-        kwargs:
-    Returns: list of funtion args"""
-    def wrapped(*args):
-        wrapped.Tup = args
-        return func(*args)
-    wrapped.Tup = (0, 0)
-    return wrapped
 
 
 class MRUCache(BaseCaching):
     """First In First Out
     """
+    MruDict = {}
+    swt = 0
+
     def __init__(self):
         """Initialize the class
         """
@@ -33,44 +22,51 @@ class MRUCache(BaseCaching):
             items: data to be stored
         Return: no return value
         """
+
         if key is not None and item is not None:
             self.cache_data[key] = item
             limit = len(self.cache_data)
+            List_keys = list(self.cache_data.keys())
+
+            if self.swt == 0:
+                for ky in List_keys:
+                    self.MruDict[key] = 0
+                self.swt = 1
+            else:
+                for ky in List_keys:
+                    if ky in self.MruDict:
+                        continue
+                    self.MruDict[ky] = 0
 
             if limit > BaseCaching.MAX_ITEMS:
-                LrUsed = self.get.Tup
-                List_keys = list(self.cache_data.keys())
+                great = self.MruDict[List_keys[0]]
+                great_key = List_keys[0]
 
-                LruDict = {}
-                # Get the keys
+                tempMruD = self.MruDict
                 for key in List_keys:
-                    LruDict[key] = 0
-                # increment values
-                for key in LruDict:
-                    if key == LrUsed[1]:
-                        LruDict[key] += 1
-                least = LruDict[List_keys[0]]
-                least_key = List_keys[0]
-
-                for key in LruDict:
-                    if least > LruDict[key]:
-                        least_key = key
-                        least = LruDict[key]
-                if List_keys[0] == least_key:
-                    self.cache_data.pop(least_key)
-                    p = least_key
+                    if great <= tempMruD[key]:
+                        great_key = key
+                        great = tempMruD[key]
+                if List_keys[0] == great_key:
+                    self.cache_data.pop(List_keys[-2])
+                    p = List_keys[-2]
                 else:
-                    self.cache_data.pop(List_keys[0])
-                    p = List_keys[0]
+                    print(self.cache_data[great_key])
+                    self.cache_data.pop(great_key)
+                    p = great_key
                 print("DISCARD: {}".format(p))
 
-    @CallCount
     def get(self, key):
         """Getter for cache_data whose value is at given key
         Args:
             key: the key to search
         Returns: the value of the cache_data
         """
+        List_keys = list(self.cache_data.keys())
+        for ky in List_keys:
+            if key == ky:
+                self.MruDict[key] += 1
         if key in self.cache_data:
             return self.cache_data[key]
+
         return None
