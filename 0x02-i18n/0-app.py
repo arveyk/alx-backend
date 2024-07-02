27478cf213963393538ef67e0f9b26f0c22801b6
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-02 13:50:46 +0300

    route return value

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return '<title>Welcome to Holbeton</title><h1>Hello World</h1>'
